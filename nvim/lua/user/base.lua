546e46c964bb11d9b8d6379defbc17a5fcaea7e6
Scott Zirkel <scott@scottzirkel.com>
2022-12-06 22:15:51 -0600

    feat(nvim): added a bunch of things i don't understan

---
+++ nvim/lua/user/base.lua
@@ -0,0 +1,16 @@
+vim.cmd("autocmd!")
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.wo.number = true
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = '*',
+    command = "set nopaste"
+})
--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -2,7 +2,14 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.hlsearch = true
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.shell = 'warp'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.title = true
@@ -10,12 +17,14 @@ vim.opt.termguicolors = true
 vim.opt.spell = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.smarttab = true
 vim.opt.wrap = false
 vim.opt.breakindent = true -- maintain indent when wrapping indented lines
 vim.opt.list = true -- enable the below listchars
 vim.opt.listchars = { tab = '▸ ', trail = '·' }
 vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
 vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.scrolloff = 8
@@ -27,7 +36,9 @@ vim.opt.backup = true -- automatically save a backup file
 vim.opt.backupdir:remove('.') -- keep backups out of the current directory
 vim.opt.shortmess:append({ I = true }) -- disable the splash screen
 vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.wildignore:append { '*/node_modules/*' }
 vim.opt.signcolumn = 'yes:2'
 vim.opt.showmode = false
 vim.opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prevent polyglot from changing it
 vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
+vim.opt.formatoptions:append { 'r' }
