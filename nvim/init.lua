0451b5dcb6b12d25850496a5a4d03f9a8dc42350
Scott Zirkel <scott@scottzirkel.com>
2022-12-07 11:49:32 -0600

    feat(nvim): added support for editorconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require('user.base')
 require('user.plugins')
 require('user.options')
+require('user.keymaps')
 require('user.misc')
 
---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>Q', ':bufo bdelete<CR>')
+
+vim.keymap.set('n', 'q:', ':q<CR>')
