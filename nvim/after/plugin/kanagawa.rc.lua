a2285e8b716ae3426a16b8d9ce794cd7ac9834da
Scott Zirkel <scott@scottzirkel.com>
2023-06-09 15:06:27 -0500

    updates

--- nvim/after/plugin/kanagawa.rc.lua
+++ nvim/after/plugin/kanagawa.rc.lua
@@ -6,19 +6,19 @@ require('kanagawa').setup({
     keywordStyle = { italic = true},
     statementStyle = { bold = true },
     typeStyle = {},
-    variablebuiltinStyle = { italic = true},
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
+    -- variablebuiltinStyle = { italic = true},
+    -- specialReturn = true,       -- special highlight for the return keyword
+    -- specialException = true,    -- special highlight for exception handling keywords
     transparent = false,        -- do not set background color
     dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
     globalStatus = false,       -- adjust window separators highlight for laststatus=3
     terminalColors = true,      -- define vim.g.terminal_color_{0,17}
     colors = {
-      sumlink1 = 'red'
+      -- sumlink1 = 'red'
     },
-    overrides = {
-      -- CursorLineBg = { fg = '#ff00ff', bg = '#00ff00' },
-    },
+    -- overrides = {
+    --   -- CursorLineBg = { fg = '#ff00ff', bg = '#00ff00' },
+    -- },
     theme = "default"           -- Load "default" theme or the experimental "light" theme
 })
 
